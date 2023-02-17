8761ef2b8bb7af7819946f270bfc797f46c57c63
Kandrw <doofydak@gmail.com>
2023-02-17 13:17:58 +0700

    Базовый интерфейс

--- Simulation/mainwindow.cpp
+++ Simulation/mainwindow.cpp
@@ -1,15 +1,54 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+#include <QtWidgets>
+#include "QTimer"
+#include <QMovie>
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setFixedSize(size_window_weight, size_window_height);
+    this->setWindowTitle("Simulation");
+
+    std::cout<<"[Create MainWindow]"<<std::endl;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_exit_program_triggered()
+{
+    QApplication::exit();
+}
+
+
+
+void MainWindow::on_button_pause_simulation_clicked()
+{
+    std::cout<<"Test pause - click"<<std::endl;
+}
+
+
+void MainWindow::on_button_create_random_object_clicked()
+{
+    std::cout<<"Test create - click"<<std::endl;
+}
+
+
+void MainWindow::on_button_clear_object_all_clicked()
+{
+    std::cout<<"Test clear - click"<<std::endl;
 }
 
--- Simulation/mainwindow.h
+++ Simulation/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 
+class QTimer;
+class QLabel;
+//class stack_object;
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -14,6 +18,40 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    int size_window_height = 800;
+    int size_window_weight = 1000;
+
+    int map_margin_from_edges_x_left = 10;
+    int map_margin_from_edges_x_right = 10;
+    int map_margin_from_edges_y_up = 100;
+    int map_margin_from_edges_y_down = 100;
+
+    int map_size_x;
+    int map_size_y;
+
+
+
+
+
+public:
+    bool pause_simulation = true;
+    int simulation_fps = 60;
+
+public:
+    QTimer *simulation_timer;
+    QLabel *simulation_map;
+
+
+private slots:
+    void on_exit_program_triggered();
+
+    void on_button_pause_simulation_clicked();
+
+    void on_button_create_random_object_clicked();
+
+    void on_button_clear_object_all_clicked();
 
 private:
     Ui::MainWindow *ui;
